d8f5e2bce0d30ea6631d734432dab2aeae1ab5af
ninaathma <nina.athma@gmail.com>
2021-02-26 14:37:39 -0800

    Initial commit

---
+++ PS2/cmake-build-cmk-docker/CMakeFiles/PS2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PS2.dir/main.c.o"
+  "PS2.pdb"
+  "PS2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PS2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
